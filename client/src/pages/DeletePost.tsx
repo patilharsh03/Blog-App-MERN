864533182efa5705bf41cce80f46230b444b617d
Harsh Patil <harshpatil223@gmail.com>
2023-04-24 17:01:06 +0530

    app completed

--- client/src/pages/DeletePost.tsx
+++ client/src/pages/DeletePost.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const DeletePost = () => {
   const navigate = useNavigate();
@@ -8,23 +8,42 @@ const DeletePost = () => {
 
   async function handleDelete() {
     const response = await fetch(`http://localhost:8000/post/${id}`, {
-        method: 'DELETE',
-        credentials: 'include'
-    })
+      method: "DELETE",
+      credentials: "include",
+    });
     if (response.ok) {
-        navigate('/')
+      navigate("/");
     } else {
-        const { message } = await response.json()
-        setError(message)
+      const { message } = await response.json();
+      setError(message);
     }
   }
 
   return (
-    <>
-        {error && <p style={{ color: "red"}}>{error}</p>}
-        <button style={{ marginTop: '10px'}} onClick={handleDelete}>Delete Post</button>
-    </>
-  )
+    <div className="delete-page">
+    <div className="delete-row">
+      {/* {error && <p style={{ color: "red"}}>{error}</p>} */}
+      <Link className="delete-btn" to={"/"} onClick={handleDelete}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+          />
+        </svg>
+        Delete Post
+      </Link>
+      {/* <button style={{ marginTop: '10px'}} onClick={handleDelete}>Delete Post</button> */}
+    </div>
+</div>
+  );
 };
 
 export default DeletePost;
